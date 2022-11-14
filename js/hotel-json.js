1f6764f7fec850e28882c1de0bfad9a91a6c77fd
HallJ8818 <111305750+HallJ8818@users.noreply.github.com>
2022-11-14 13:18:49 -0500

    1 file modified

--- js/hotel-json.js
+++ js/hotel-json.js
@@ -1,14 +1,13 @@
 /* Change the configuration */
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM',
-  authDomain: 'grocerylist-91956.firebaseapp.com',
-  databaseURL: 'https://grocerylist-91956.firebaseio.com',
-  projectId: 'grocerylist-91956',
-  storageBucket: 'grocerylist-91956.appspot.com',
-  messagingSenderId: '813812426276',
-  appId: '1:813812426276:web:93e5897af12892ff78dab1',
-  measurementId: 'G-VZ83BTR72T',
+  apiKey: "AIzaSyBC8RZY7dcUF2OTpe8TF30QOUBjAVRaWKc",
+  authDomain: "grocerylist-8b594.firebaseapp.com",
+  projectId: "grocerylist-8b594",
+  storageBucket: "grocerylist-8b594.appspot.com",
+  messagingSenderId: "871764817573",
+  appId: "1:871764817573:web:12fb4afd10b423a69a7f4c",
+  measurementId: "G-YS8JY5WKP6"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -16,8 +15,18 @@ firebase.initializeApp(firebaseConfig);
 // enter data in
 $("input[type='button']").click(function (e) {
   //get the value of form
-
+  var inputdata = $('form').serializeArray();
+  var data = {};
+  console.log(inputdata[2]);
+  console.log(inputdata[2].name);
+  console.log(inputdata[2].value);
   /* save the data to database */
+  inputdata.forEach((entry) =>{
+    console.log(entry);
+    data[entry.name]=entry.value;
+  });
+  //console.log(data);
+  firebase.firestore().collection("hotel").add({data});
 
   /* clear the entry */
   $('form')[0].reset();
@@ -31,16 +40,16 @@ array1.forEach(element => console.log(element));
 
 /* read the data from the database */
 
-/*
+
 firebase
   .firestore()
-  .collection('hoteldata')
+  .collection('hotel')
   .onSnapshot((querySnapshot) => {
     console.log(querySnapshot.size);
     querySnapshot.forEach((doc) => {
-      console.log(doc.data());
-      console.log(doc.data().room);
-      console.log(doc.data().checkout);
+      console.log(doc.data().data);
+      console.log(doc.data().data.room);
+      console.log(doc.data().data.checkout);
     });
   });
-*/
+
