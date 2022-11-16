219a9ee641659e516bba5f6302abb21a0c2603a2
HallJ8818 <111305750+HallJ8818@users.noreply.github.com>
2022-11-16 13:05:24 -0500

    2 files modified

--- js/SaveDisplay.js
+++ js/SaveDisplay.js
@@ -1,6 +1,14 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {};
+var firebaseConfig = {
+  apiKey: "AIzaSyBC8RZY7dcUF2OTpe8TF30QOUBjAVRaWKc",
+  authDomain: "grocerylist-8b594.firebaseapp.com",
+  projectId: "grocerylist-8b594",
+  storageBucket: "grocerylist-8b594.appspot.com",
+  messagingSenderId: "871764817573",
+  appId: "1:871764817573:web:12fb4afd10b423a69a7f4c",
+  measurementId: "G-YS8JY5WKP6"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -9,10 +17,38 @@ $(".sampleSurvey input[type='submit']").click(function(e) {
   e.preventDefault();
 
   // get the value of the form using serializeArray method
-  
+  var inputdata = $('form').serializeArray();
+  var data = {};
 
+  inputdata.forEach((entry) =>{
+    data[entry.name]=entry.value;
+  });
 
+  firebase.firestore().collection("surveydata").add({data});
 
+  /* clear the entry */
+  $('form')[0].reset();
 });
 
 // update the result in table
+firebase.firestore().collection("surveydata").onSnapshot(function(querySnapShot){
+  var n1 = 0; // how many A's
+  var n2 = 0; // how many B's
+  var n3 = 0; // how many C's
+  var n4 = 0; // how many D's
+  var n5 = 0; // how many E's
+  querySnapShot.forEach(function(doc){
+    console.log("document -- ", doc.data().data.choice);
+    var s = doc.data().data.choice;
+    switch(s){
+      case "A": n1++; $('#ans1').text(n1);break;
+      case "B": n2++; $('#ans2').text(n2);break;
+      case "C": n3++; $('#ans3').text(n3);break;
+      case "D": n4++; $('#ans4').text(n4);break;
+      case "E": n5++; $('#ans5').text(n5);break;
+    }
+
+  });
+  console.log("n1="+ n1 +" n2="+ n2 +" n3="+ n3 + " n4="+ n4 + " n5="+ n5)
+
+});
--- js/Signup.js
+++ js/Signup.js
@@ -16,8 +16,8 @@ $("#signup-form").submit(function(e) {
   e.preventDefault();
   // get the username(email) and password from the form
   // change the following code
-  var email = "yilianz4@gmail.com";
-  var password = "ddsgagafda";
+  var email = "effhall01@gmail.com";
+  var password = "12345";
 
   // create a user with email address and password
   firebase
